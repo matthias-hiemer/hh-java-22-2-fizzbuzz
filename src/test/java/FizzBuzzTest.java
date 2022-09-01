b0002d52c8fe44928a862c1771f22dc84edbcabe
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-09-01 10:00:46 +0200

    Add checks for multiple of 3 and 5 and 15

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,6 +5,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FizzBuzzTest {
 
     @Test
+    void fizzBuzzTestFor3(){
+        // GIVEN
+        int number = 3;
+
+        // WHEN
+        String actual = FizzBuzz.fizzBuzz(number);
+
+        // THEN
+        assertEquals("fizz", actual);
+    }
+
+    @Test
+    void fizzBuzzTestFor5(){
+        // GIVEN
+        int number = 5;
+
+        // WHEN
+        String actual = FizzBuzz.fizzBuzz(number);
+
+        // THEN
+        assertEquals("buzz", actual);
+    }
+
+    @Test
+    void fizzBuzzTestFor6(){
+        // GIVEN
+        int number = 6;
+
+        // WHEN
+        String actual = FizzBuzz.fizzBuzz(number);
+
+        // THEN
+        assertEquals("fizz", actual);
+    }
+
+    @Test
     void fizzBuzzTestFor4(){
         // GIVEN
         int number = 4;
@@ -15,5 +51,31 @@ class FizzBuzzTest {
         // THEN
         assertEquals("4", actual);
     }
+
+
+    @Test
+    void fizzBuzzTestFor15(){
+        // GIVEN
+        int number = 15;
+
+        // WHEN
+        String actual = FizzBuzz.fizzBuzz(number);
+
+        // THEN
+        assertEquals("fizzbuzz", actual);
+    }
+
+    @Test
+    void fizzBuzzTestFor75(){
+        // GIVEN
+        int number = 75;
+
+        // WHEN
+        String actual = FizzBuzz.fizzBuzz(number);
+
+        // THEN
+        assertEquals("fizzbuzz", actual);
+    }
+
 
 }
