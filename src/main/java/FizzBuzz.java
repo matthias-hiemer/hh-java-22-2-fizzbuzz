066869f31a6394653194bd6677dadb63e62aedd5
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-09-01 10:00:38 +0200

    Add checks for multiple of 3 and 5 and 15

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,9 +3,23 @@ public class FizzBuzz {
 
     public static String fizzBuzz(int number){
 
+        boolean isDividableBy3 = number % 3 == 0;
+        boolean isDividableBy5 = number % 5 == 0;
+
+        if(isDividableBy3 && isDividableBy5){
+            return "fizzbuzz";
+
+        } else if (isDividableBy3) {
+            return "fizz";
+
+        } else if (isDividableBy5) {
+            return "buzz";
+
+        } else {
+            return String.valueOf(number);
+        }
 
 
-        return String.valueOf(number);
     }
 
 }
